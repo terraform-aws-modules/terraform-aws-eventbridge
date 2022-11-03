5391634401526faf6a5de7b925518d359e402352
Ryan Bouamara <ryan.bouamara@epitech.eu>
2022-11-03 13:51:52 +0100

    Fix Outputs
    Add eventbridge_api_destination_ids and fix eventbridge_api_destination_arns to return arn instead of ids

--- outputs.tf
+++ outputs.tf
@@ -35,6 +35,11 @@ output "eventbridge_connection_arns" {
 # EventBridge Destination
 output "eventbridge_api_destination_arns" {
   description = "The EventBridge API Destination ARNs created"
+  value       = { for k, v in aws_cloudwatch_event_api_destination.this : k => v.arn }
+}
+
+output "eventbridge_api_destination_ids" {
+  description = "The EventBridge API Destination IDs created"
   value       = { for k, v in aws_cloudwatch_event_api_destination.this : k => v.id }
 }
 
