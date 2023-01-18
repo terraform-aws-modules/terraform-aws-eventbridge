03ef4ff5927259b53c4b10c9d90d39db78e80196
Howard Zhou <lovemew67@gmail.com>
2023-01-18 17:11:58 +0800

    fix: Wrong value of api destination output (#79)
    Co-authored-by: howardzhou <howardzhou@kkstream.com>

--- outputs.tf
+++ outputs.tf
@@ -35,7 +35,7 @@ output "eventbridge_connection_arns" {
 # EventBridge Destination
 output "eventbridge_api_destination_arns" {
   description = "The EventBridge API Destination ARNs created"
-  value       = { for k, v in aws_cloudwatch_event_api_destination.this : k => v.id }
+  value       = { for k, v in aws_cloudwatch_event_api_destination.this : k => v.arn }
 }
 
 # EventBridge Rule
