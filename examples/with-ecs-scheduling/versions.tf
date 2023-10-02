ba4d055d0aabb16e356934c594ad26cc2057f058
Anton Babenko <anton@antonbabenko.com>
2023-10-02 13:19:15 +0200

    feat!: Upgraded AWS provider to v5 (required for Pipes) (#94)

--- examples/with-ecs-scheduling/versions.tf
+++ examples/with-ecs-scheduling/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.64"
+      version = ">= 5.9"
     }
     random = {
       source  = "hashicorp/random"
